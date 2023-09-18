d81982de1ea972f95f3b50a15c5e1b647237f985
awxkee <qnljoy@gmail.com>
2023-09-18 22:50:18 +0800

    Initial release

---
+++ jxlcodercoil/build.gradle.kts
@@ -0,0 +1,67 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    id("maven-publish")
+}
+
+task("androidSourcesJar", Jar::class) {
+    archiveClassifier.set("sources")
+    from(android.sourceSets.getByName("main").java.srcDirs)
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("mavenJava") {
+                groupId = "com.github.awxkee"
+                artifactId = "jxl-coder-coil"
+                version = "1.5.0"
+                from(components.findByName("release"))
+//                artifact("androidSourcesJar")
+            }
+        }
+    }
+}
+
+
+android {
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+            withJavadocJar()
+        }
+    }
+
+    namespace = "com.awxkee.jxlcoder.coil"
+    compileSdk = 33
+
+    defaultConfig {
+        minSdk = 21
+        targetSdk = 33
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    api("io.coil-kt:coil:2.4.0")
+    api("com.github.awxkee:jxl-coder:1.+")
+}
---
+++ jxlcodercoil/src/main/java/com/awxkee/jxlcoder/coil/JxlDecoder.kt
@@ -0,0 +1,113 @@
+package com.awxkee.jxlcoder.coil
+
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
+import android.os.Build
+import coil.ImageLoader
+import coil.decode.DecodeResult
+import coil.decode.Decoder
+import coil.fetch.SourceResult
+import coil.request.Options
+import coil.size.Scale
+import coil.size.pxOrElse
+import com.awxkee.jxlcoder.JxlCoder
+import com.awxkee.jxlcoder.PreferredColorConfig
+import com.awxkee.jxlcoder.ScaleMode
+import kotlinx.coroutines.runInterruptible
+import okio.BufferedSource
+import okio.ByteString.Companion.toByteString
+
+class JxlDecoder(
+    private val source: SourceResult,
+    private val options: Options,
+    private val imageLoader: ImageLoader
+) : Decoder {
+
+    override suspend fun decode(): DecodeResult? = runInterruptible {
+        // ColorSpace is preferred to be ignored due to lib is trying to handle all color profile by itself
+        val sourceData = source.source.source().readByteArray()
+
+        var mPreferredColorConfig: PreferredColorConfig = when (options.config) {
+            Bitmap.Config.ALPHA_8 -> PreferredColorConfig.RGBA_8888
+            Bitmap.Config.RGB_565 -> if (options.allowRgb565) PreferredColorConfig.RGB_565 else PreferredColorConfig.DEFAULT
+            Bitmap.Config.ARGB_8888 -> PreferredColorConfig.RGBA_8888
+            else -> PreferredColorConfig.DEFAULT
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O && options.config == Bitmap.Config.RGBA_F16) {
+            mPreferredColorConfig = PreferredColorConfig.RGBA_F16
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q && options.config == Bitmap.Config.HARDWARE) {
+            mPreferredColorConfig = PreferredColorConfig.HARDWARE
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU && options.config == Bitmap.Config.RGBA_1010102) {
+            mPreferredColorConfig = PreferredColorConfig.RGBA_1010102
+        }
+
+        if (options.size == coil.size.Size.ORIGINAL) {
+            val originalImage =
+                JxlCoder().decode(
+                    sourceData,
+                    preferredColorConfig = mPreferredColorConfig
+                )
+            return@runInterruptible DecodeResult(
+                BitmapDrawable(
+                    options.context.resources,
+                    originalImage
+                ), false
+            )
+        }
+
+        val dstWidth = options.size.width.pxOrElse { 0 }
+        val dstHeight = options.size.height.pxOrElse { 0 }
+        val scaleMode = when (options.scale) {
+            Scale.FILL -> ScaleMode.FILL
+            Scale.FIT -> ScaleMode.FIT
+        }
+
+        val originalImage =
+            JxlCoder().decodeSampled(
+                sourceData,
+                dstWidth,
+                dstHeight,
+                preferredColorConfig = mPreferredColorConfig,
+                scaleMode,
+            )
+        return@runInterruptible DecodeResult(
+            BitmapDrawable(
+                options.context.resources,
+                originalImage
+            ), true
+        )
+    }
+    class Factory : Decoder.Factory {
+        override fun create(
+            result: SourceResult,
+            options: Options,
+            imageLoader: ImageLoader
+        ) = if (isJXL(result.source.source())) {
+            JxlDecoder(result, options, imageLoader)
+        } else null
+
+        private val MAGIC_1 = byteArrayOf(0xFF.toByte(), 0x0A).toByteString()
+        private val MAGIC_2 = byteArrayOf(
+            0x0.toByte(),
+            0x0.toByte(),
+            0x0.toByte(),
+            0x0C.toByte(),
+            0x4A,
+            0x58,
+            0x4C,
+            0x20,
+            0x0D,
+            0x0A,
+            0x87.toByte(),
+            0x0A
+        ).toByteString()
+
+        private fun isJXL(source: BufferedSource): Boolean {
+            return source.rangeEquals(0, MAGIC_1) || source.rangeEquals(
+                0,
+                MAGIC_2
+            )
+        }
+    }
+
+}

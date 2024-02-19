21e5b6ee725cc73ffdad5efd214390cf1555a388
awxkee <radzivon.bartoshyk@proton.me>
2024-02-19 09:16:27 +0000

    Release 1.10.2

--- jxlcodercoil/build.gradle.kts
+++ jxlcodercoil/build.gradle.kts
@@ -62,5 +62,5 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     api("io.coil-kt:coil:2.5.0")
-    api("com.github.awxkee:jxl-coder:1.10.1")
+    api("com.github.awxkee:jxl-coder:1.10.2")
 }

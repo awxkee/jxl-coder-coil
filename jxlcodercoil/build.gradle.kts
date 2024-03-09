fd7dd6157dbcf47bb43bcaa75ce91db9bc752425
awxkee <radzivon.bartoshyk@proton.me>
2024-03-09 18:22:13 +0000

    Upgrade to 2.1.2 jxl-coder

--- jxlcodercoil/build.gradle.kts
+++ jxlcodercoil/build.gradle.kts
@@ -62,5 +62,5 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     api("io.coil-kt:coil:2.6.0")
-    api("com.github.awxkee:jxl-coder:2.1.1")
+    api("com.github.awxkee:jxl-coder:2.1.2")
 }
